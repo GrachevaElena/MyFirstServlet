90907b0de7ab0bd8722df397062461579b7dfd4d
Alyona <alyona-gra98@yandex.ru>
2017-11-20 23:16:26 +0300

    Task3

--- src/com/netcracker/MyServlet.java
+++ src/com/netcracker/MyServlet.java
@@ -7,41 +7,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 /**
  * Created by glak0514 on 14.11.2017.
  */
-@WebServlet("/authorization")
+@WebServlet("/mainForm")
 public class MyServlet extends HttpServlet {
 
-    private static String message = "Hello from my first servlet";
-    private static final String password="123456";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String userName = req.getParameter("username");
-        String userPassword = req.getParameter("password");
-
-        PrintWriter pw = resp.getWriter();
-
-        StringBuilder builder = new StringBuilder();
+        StringBuilder builder=new StringBuilder();
 
         builder.append("<html>");
         builder.append("<body>");
-
-        if (userPassword.equals(password))
-            builder.append("<h2> Hello, "+ userName+ "</br>");
-        else {
-            builder.append("<form name=\"loginForm\" method=\"GET\" action=\"login.html\">");
-            builder.append("<p> Invalid password, press enter to return </p>");
-            builder.append("<input type=\"submit\" value=\"Return\">");
-            builder.append(" </form>");
-        }
-
+        builder.append("<form>");
+        builder.append("Data/time: "+new Date().toString()+"</br>");
+        builder.append("Browser: "+req.getHeader("user-agent")+"</br>");
+        builder.append("<input type=\"submit\" value=\"Refresh\">");
+        builder.append("</form>");
         builder.append("</body>");
         builder.append("</html>");
 
+
+
+        PrintWriter pw = resp.getWriter();
         pw.write(builder.toString());
 
     }
