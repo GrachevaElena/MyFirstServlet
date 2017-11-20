35a47199c16f00c31487f50efcbad16338c4c7c1
Alyona <alyona-gra98@yandex.ru>
2017-11-21 01:51:02 +0300

    try send

--- src/com/netcracker/MyServlet.java
+++ src/com/netcracker/MyServlet.java
@@ -1,53 +1,50 @@
 package com.netcracker;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-/**
- * Created by glak0514 on 14.11.2017.
- */
-@WebServlet("/authorization")
+import javax.mail.*;
+import java.util.Properties;
+
+
+
+@WebServlet("/send")
 public class MyServlet extends HttpServlet {
-
-    private static String message = "Hello from my first servlet";
-    private static final String password="123456";
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String userName = req.getParameter("username");
-        String userPassword = req.getParameter("password");
-
-        PrintWriter pw = resp.getWriter();
-
-        StringBuilder builder = new StringBuilder();
-
-        builder.append("<html>");
-        builder.append("<body>");
-
-        if (userPassword.equals(password))
-            builder.append("<h2> Hello, "+ userName+ "</br>");
-        else {
-            builder.append("<form name=\"loginForm\" method=\"GET\" action=\"login.html\">");
-            builder.append("<p> Invalid password, press enter to return </p>");
-            builder.append("<input type=\"submit\" value=\"Return\">");
-            builder.append(" </form>");
-        }
-
-        builder.append("</body>");
-        builder.append("</html>");
-
-        pw.write(builder.toString());
-
-    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.doGet(req, resp);
+
+        String email=req.getParameter("email");
+        String text=req.getParameter("textarea");
+        String sub=req.getParameter("subject");
+
+        Properties mailProps = new Properties();
+
+        mailProps.put("mail.transport.protocol","smtp");
+        mailProps.put("mail.smtp.host","smtp.yandex.ru");
+        mailProps.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(mailProps,new Authenticator(){
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return(new PasswordAuthentication("testMailJavaMail","1324354657"));
+            }
+        });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("testMailJavaMail@yandex.ru"));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+
+            message.setSubject(sub, "koi8-r");
+            message.setText(text, "koi8-r");
+            
+            Transport.send(message);
+        } catch (MessagingException m){ m.printStackTrace();}
+
     }
 }
